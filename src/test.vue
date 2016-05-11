70f62cdfc74ec5a5e24b1d94413fc3d79e957932
Jeffrey <jeffreywangxiao@gmail.com>
2016-05-11 19:06:49 +0800

    initialize

---
+++ src/test.vue
@@ -0,0 +1,19 @@
+<style>
+body{
+    background-color: #ccc;
+}
+</style>
+<template>
+    <p v-on:click="test">test.vue loaded</p>
+</template>
+<script>
+
+module.exports = {
+    methods: {
+        test: function () {
+            console.log('run it.');
+        }
+    }
+} 
+
+</script>
